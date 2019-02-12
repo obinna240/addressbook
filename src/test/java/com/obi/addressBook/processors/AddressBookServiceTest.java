c74bb2959462730ba67d53e1bc2a85ac9d23c99f
Obinna.Onyimadu <obinna.onyimadu@sap.com>
2019-02-12 14:50:56 +0000

    adding more test instances

--- src/test/java/com/obi/addressBook/processors/AddressBookServiceTest.java
+++ src/test/java/com/obi/addressBook/processors/AddressBookServiceTest.java
@@ -1,5 +1,27 @@
 package com.obi.addressBook.processors;
 
+import org.junit.Test;
+
 public class AddressBookServiceTest {
 
+	@Test
+	public void returns6ForCountOfMalesInAddressBook() {
+		//create helper class to read addresses
+		//run method to count males
+		//perform assertion
+	}
+	
+	@Test
+	public void returns5ForCountOfFemalesInAddressBook() {
+		//create helper class to read addresses
+		//run method to count femmales
+		//perform assertion
+	}
+	
+	@Test
+	public void returns0ForCountOfMalesWhenAddressBookIsEmpty() {
+		//create helper class to read addresses from addressbook
+		//run method to count males
+		//perform assertion
+	}
 }
