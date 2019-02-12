c794c812d72e989dbba77806e09290bceabe1f9d
Obinna.Onyimadu <obinna.onyimadu@sap.com>
2019-02-12 15:27:05 +0000

    master | Test addressbook is empty

--- src/test/java/com/obi/addressBook/processors/ReaderTest.java
+++ src/test/java/com/obi/addressBook/processors/ReaderTest.java
@@ -1,5 +1,6 @@
 package com.obi.addressBook.processors;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -15,11 +16,8 @@ import org.junit.Test;
 public class ReaderTest {
 
 	@Test
-	public void returnsEmptyListWhenItReadsNonExistentAddressBook() throws IOException, URISyntaxException {
-		//reader parses the address book
-		//reader returns list
-		//we assert
-		Reader testReader = new Reader("NonExistentAddressBook");
+	public void returnsEmptyListWhenItReadsEmptyAddressBook() throws IOException, URISyntaxException {
+		Reader testReader = new Reader("EmptyAddressBook");
 		assertTrue(testReader.parseAddressBookContent().isEmpty());
 	}
 }
