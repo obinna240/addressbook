db335920f01e8448a7fd2892ec45420a10fa2362
Obinna.Onyimadu <obinna.onyimadu@sap.com>
2019-02-12 16:06:00 +0000

    master | cleanup

--- src/test/java/com/obi/addressBook/processors/ReaderTest.java
+++ src/test/java/com/obi/addressBook/processors/ReaderTest.java
@@ -18,8 +18,8 @@ import org.junit.rules.ExpectedException;
  */
 public class ReaderTest {
 	
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+//	@Rule
+//	public ExpectedException thrown = ExpectedException.none();
 	
 	@Test
 	public void returnsEmptyListWhenItReadsEmptyAddressBook() throws IOException, URISyntaxException {
