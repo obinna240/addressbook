70fecbde8035856d621946823de4e5558f489b83
Obinna.Onyimadu <obinna.onyimadu@sap.com>
2019-02-12 16:03:12 +0000

    master | reader test to include exceptions for non existent addressbooks

--- src/test/java/com/obi/addressBook/processors/ReaderTest.java
+++ src/test/java/com/obi/addressBook/processors/ReaderTest.java
@@ -2,11 +2,14 @@ package com.obi.addressBook.processors;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import java.util.NoSuchElementException;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * 
@@ -14,10 +17,19 @@ import org.junit.Test;
  *
  */
 public class ReaderTest {
-
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
 	public void returnsEmptyListWhenItReadsEmptyAddressBook() throws IOException, URISyntaxException {
 		Reader testReader = new Reader("EmptyAddressBook");
 		assertTrue(testReader.parseAddressBookContent().isEmpty());
+	}
+	
+	@Test(expected = NoSuchElementException.class) 
+	public void throwsNoSuchElementExceptionForNonExistentAddressBook() throws IOException, URISyntaxException {
+		Reader testReader = new Reader("NonExistentBook");
+		testReader.parseAddressBookContent();
 	}
 }
