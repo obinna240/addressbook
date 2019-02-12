04458e521a824a7d94ade79d42ab0c105c34970e
Obinna.Onyimadu <obinna.onyimadu@sap.com>
2019-02-12 15:08:55 +0000

    Master | Tests for reader

---
+++ src/test/java/com/obi/addressBook/processors/ReaderTest.java
@@ -0,0 +1,18 @@
+package com.obi.addressBook.processors;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Dr Obinna Onyimadu
+ *
+ */
+public class ReaderTest {
+
+	@Test
+	public void returnsZeroWhenItReadsEmptyAddressBook() {
+		//reader parses the address book
+		//reader returns list
+		//we assert
+	}
+}
