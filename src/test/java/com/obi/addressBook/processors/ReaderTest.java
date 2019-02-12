cba037ddb7aa622068a8d317a1388b00338910d1
Obinna.Onyimadu <obinna.onyimadu@sap.com>
2019-02-12 16:12:14 +0000

    master | cleanup

--- src/test/java/com/obi/addressBook/processors/ReaderTest.java
+++ src/test/java/com/obi/addressBook/processors/ReaderTest.java
@@ -1,16 +1,12 @@
 package com.obi.addressBook.processors;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import java.util.NoSuchElementException;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
-
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+
 
 /**
  * 
