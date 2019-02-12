dfa0c6a88665101030a7ede70090fec0dcabc3f1
Obinna.Onyimadu <obinna.onyimadu@sap.com>
2019-02-12 15:03:54 +0000

    Master | App runner

---
+++ src/main/java/com/obi/addressBook/App.java
@@ -0,0 +1,13 @@
+package com.obi.addressBook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
